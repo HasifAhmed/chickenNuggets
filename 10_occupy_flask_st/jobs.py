311cd9ee075f81b98bbedfa58d99a923eb1445a5
unknown <hahmed@stuy.edu>
2018-09-21 23:43:06 -0700

    basic

--- 10_occupy_flask_st/jobs.py
+++ 10_occupy_flask_st/jobs.py
@@ -7,7 +7,7 @@ def homepage():
     return "This is the homepage"
 
 @app.route('/occupations')
-def joblist(hello = NONE, foo = NONE   ):
+def joblist(hello = None, foo = None  ):
     return render_template('occ.html', hello = "hello", foo = "foo")
 
 app.debug = True
