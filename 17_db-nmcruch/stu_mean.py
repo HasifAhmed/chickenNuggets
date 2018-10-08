e4a70a24cd95b157e67e6d61c12cb2e77043a52d
unknown <hahmed@stuy.edu>
2018-10-08 15:14:43 -0700

    average plus comments

--- 17_db-nmcruch/stu_mean.py
+++ 17_db-nmcruch/stu_mean.py
@@ -59,17 +59,46 @@ createtable('courses.csv', 'courses_info')
 db.commit()  # save changes
 db.close()  # close database
 
-db = sqlite3.connect(DB_FILE)
-c = db.cursor()
 
-def createDict():
-    DICT = {}
-    info = {}
-    c.execute("SELECT name, peeps_info.id, mark FROM peeps_info, courses_info WHERE peeps_info.id = courses_info.id;")
+db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+c = db.cursor()  # facilitate db ops
+
+def createAvgtbl():
+    avg = {} 
+    count = []
+    
+    names = c.execute("SELECT name FROM peeps_info;") #pull the names of the students and put them into a list
+    for n in names:
+        #print(n[0]) #testing
+        count.append(n[0])
+
+    for name in count: #iterate through the list of names 
+        total = 0
+        num = 0.0
+        #Have to execute every single time you want to iterate through the following data, or else it doesnt work
+        info = c.execute("SELECT name, peeps_info.id, mark FROM peeps_info, courses_info WHERE peeps_info.id = courses_info.id;")
+        for row in info: #iterate through the data
+            if(name == row[0]): #if the name from the list matches the name from the row
+                #print("Hello") #testing
+                total += int(row[2]) #calculate the sum
+                num += 1.0
+        if( num != 0): #to avoid dividing by 0
+            total /= num #calculate avg
+            avg[name] = total #put into the dictionary
+            #print(avg[name]) #testing
+            
+    c.execute("CREATE TABLE IF NOT EXISTS peeps_avg(name BLOB, average BLOB);") #create peeps_avg
+    for k,v in avg.items(): #iterate through dictrionary with items() so u can access key and value 
+        vals = ""
+        vals += "'{0}'".format(k) + "," + "'{0}'".format(v) #what you are inputting 
+        command = "INSERT INTO peeps_avg VALUES({0});".format(vals) #insert them 
+        c.execute(command) #execute command 
+    
+        
     
     
 
-createDict() 
+createAvgtbl()
 db.commit()  # save changes
 db.close()  # close database
 
